c5a52369e6bc29ce3b8d0e9a5d96d750b27847d3
vishalghag <vishalghag47@gmail.com>
2024-05-03 08:09:40 +0530

    last drag and drop remain rest done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { DevTools } from "jotai-devtools";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <DevTools position="bottom-right" />
     </div>
   );
 }
